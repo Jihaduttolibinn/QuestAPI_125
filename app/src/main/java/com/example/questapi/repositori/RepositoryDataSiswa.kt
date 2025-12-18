3cfb0f0595d2b4fef52dd6d0a0a3360fffe90f45
jihaduttolibinn <jihaduttolibin02@gmail.com>
2025-12-18 19:51:28 +0700

    Menambahkan RepositoryDataSiswa.kt

---
+++ app/src/main/java/com/example/questapi/repositori/RepositoryDataSiswa.kt
@@ -0,0 +1,19 @@
+package com.example.questapi.repositori
+
+
+import com.example.questapi.apiservice.ServiceApiSiswa
+import com.example.questapi.modeldata.DataSiswa
+
+
+interface RepositoryDataSiswa {
+    suspend fun getDataSiswa(): List<DataSiswa>
+    suspend fun postDataSiswa(dataSiswa: DataSiswa):retrofit2.Response<Void>
+}
+
+class jaringanRepositoryDataSiswa (
+    private val serviceApiSiswa: ServiceApiSiswa
+): RepositoryDataSiswa{
+    override suspend fun getDataSiswa(): List<DataSiswa> = serviceApiSiswa.getSiswa()
+    override suspend fun postDataSiswa(dataSiswa: DataSiswa):retrofit2
+    .Response<Void> = serviceApiSiswa.postSiswa(dataSiswa)
+}
